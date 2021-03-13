e0abc80948c4ae7bd24d4bd83672166deade2eff
vinay <myanavinay1234@gmail.com>
2021-03-13 23:04:56 +0530

    third commit

---
+++ crudfbvproject/crudfbvproject/urls.py
@@ -0,0 +1,25 @@
+"""crudfbvproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from testapp import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('re/', views.retrieve_view),
+    path('create/', views.create_view),
+    path('delete/(?P<id>\d+)/re', views.delete_view),
+    path('update/(?p<id>\d+)/re', views.update_view)
+]
